edacce677d96716dfc37a7c7cb17c67fcbd1603e
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-05-24 17:33:06 +0100

    Add error handling to install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,10 @@
 title AdFly-Bot installer
 color 0a
+where python
+if %errorlevel% neq 0 (
+	echo Python is not installed or it is not in PATH.
+	echo You can install it from https://www.python.org/
+	exit 1
+)
 pip install -r requirements.txt
 echo Successfully installed AdFly-Bot.
