231d76cd7911fc79ac3c0c83c5e4d42a76da48fc
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-07-20 20:08:36 +0100

    Major update

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # AdFly-Bot
 
 ![Made with Python](https://img.shields.io/badge/made%20with-python-0.svg?color=cc2020&labelColor=ff3030&logo=python&logoColor=white&style=for-the-badge)
-![Made with Bash](https://img.shields.io/badge/made%20with-bash-0.svg?color=cc2020&labelColor=ff3030&style=for-the-badge&logo=data%3Aimage%2Fsvg%2Bxml%3Bbase64%2CPHN2ZyB2ZXJzaW9uPSIxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1MTIgNTEyIj48cGF0aCBkPSJNNTEyIDI1NmEyNTUgMjU1IDAgMCAxLTI4IDExNyAyNDkgMjQ5IDAgMCAxLTczIDg3IDI1NiAyNTYgMCAwIDEtODkgNDMgMjQ4IDI0OCAwIDAgMS0xMDYgNkEyNTMgMjUzIDAgMCAxIDI1IDM2N2EyNTYgMjU2IDAgMSAxIDQ4Ny0xMTF6IiBmaWxsPSIjZjJmMmYyIi8%2BPGcgZmlsbD0iIzJkM2EzZSI%2BPHBhdGggZD0iTTQ0NSA0MjVsLTE0NCA4MyAyMS01YzEyLTMgMjItNiAzMy0xMWw1Ni0zMmEyNTggMjU4IDAgMCAwIDQyLTQxbC04IDZ6bTctMzAyTDI5NiAzM2E3NyA3NyAwIDAgMC03NiAwTDY0IDEyM2E3NyA3NyAwIDAgMC0zOSA2N3YxNzdjMTYgMzMgMzkgNjIgNjcgODVsNzcgNDVjMTUgNSAzMSA5IDQ3IDEyTDcwIDQyNWE2NCA2NCAwIDAgMS0zMS01NVYxOTBjMC0yMyAxMi00NCAzMS01NWwxNTYtOTBhNjMgNjMgMCAwIDEgNjQgMGwxNTUgOTBhNjQgNjQgMCAwIDEgMzIgNTV2MTgwYzAgNy0xIDE1LTQgMjJsMTEtMTkgNi0xNFYxOTBjMC0yNy0xNC01My0zOC02N3oiLz48cGF0aCBkPSJNNDg0IDE5MHYxODNhMjQ5IDI0OSAwIDAgMS00NyA2NGwtMTE1IDY2YTI0OCAyNDggMCAwIDEtNzEgOWwtMi0xMVYzMjFjMC0yNSAxMy00OCAzNS02MWwxNTYtOTAgOC00YTI2IDI2IDAgMCAxIDM2IDI0eiIvPjwvZz48cGF0aCBkPSJNMzQ4IDM4NWMwLTE4LTE2LTE4LTI0LTE4cy0xMy0zLTEzLTExIDEwLTIwIDI4LTE3bDctMThzLTgtMS0xOCAxdi0xN2wtMTUgOXYxNWMtOSA2LTE3IDE5LTE3IDQxIDAgMTAgNSAyMCAyMiAyMCAxNiAwIDE3IDE1IDggMjMtOSA5LTI5IDktMjkgOWwtMiAyMHM4IDAgMTgtM3YxN2wxNS05di0xNWMxMS04IDIxLTIyIDIwLTQ3eiIgZmlsbD0iI2ZmZiIvPjxwYXRoIGZpbGw9IiMzMWI2NDQiIGQ9Ik00MTMgNDA5bC00MyAyOXYtMTZsNDMtMjh6Ii8%2BPC9zdmc%2B)
-![Made with Batch](https://img.shields.io/badge/made%20with-batch-0.svg?color=cc2020&labelColor=ff3030&style=for-the-badge)
 
 ![GitHub](https://img.shields.io/github/license/DeBos99/adfly-bot.svg?color=2020cc&labelColor=5050ff&style=for-the-badge)
 ![GitHub followers](https://img.shields.io/github/followers/DeBos99.svg?color=2020cc&labelColor=5050ff&style=for-the-badge)
@@ -26,11 +24,13 @@
 ## Content
 
 - [Content](#content)
-- [Prerequisites](#prerequisites)
-  - [Windows](#Prerequisites-Windows)
 - [Installation](#installation)
-  - [Windows](#Installation-Windows)
-  - [Linux & MacOS](#Linux&MacOS)
+  - [Windows](#windows)
+  - [Unix](#unix)
+    - [Debian/Ubuntu](#apt)
+    - [Arch Linux/Manjaro](#pacman)
+    - [CentOS](#yum)
+    - [MacOS](#homebrew)
 - [Usage](#usage)
 - [Documentation](#documentation)
   - [Required arguments](#required-arguments)
@@ -40,40 +40,52 @@
 - [Contact](#contact)
 - [License](#license)
 
-## Prerequisites
-
-### <a name="Prerequisites-Windows">Windows
-
-Install **Google Chrome Browser**: https://www.google.com/chrome/
-
-Download **ChromeDriver** and move the executable to folder in your **PATH**: http://chromedriver.chromium.org/downloads
-
-Or
-
-Install **Mozilla Firefox Browser**: https://www.mozilla.org/en-US/firefox/new/
-
-Download **geckodriver**, unzip and move the executable to folder in your **PATH**: https://github.com/mozilla/geckodriver/releases
-
 ## Installation
 
-### <a name="Installation-Windows">Windows
+### Windows
 
-Install Python: https://www.python.org/downloads/
-
+* Install [Git](https://git-scm.com/download/win).
+* Install [Python](https://www.python.org/downloads/).
+* Install [Google Chrome](https://www.google.com/chrome/).
+<br>Or
+<br>Install [Firefox](https://www.mozilla.org/firefox/new/).
+* Run following command in the command prompt:
 ```
 git clone "https://github.com/DeBos99/adfly-bot.git"
-cd adfly-bot
-install.bat
-cd ..
 ```
 
-### <a name="Linux&MacOS">Linux & MacOS
+### Unix
 
+#### <a name="APT">Debian/Ubuntu based
+
+* Run following commands in the terminal:
 ```
+sudo apt install git python -y
 git clone "https://github.com/DeBos99/adfly-bot.git"
-cd adfly-bot
-./install.sh
-cd ..
+```
+
+#### <a name="Pacman">Arch Linux/Manjaro
+
+* Run following commands in the terminal:
+```
+sudo pacman -S git python --noconfirm
+git clone "https://github.com/DeBos99/adfly-bot.git"
+```
+
+#### <a name="YUM">CentOS
+
+* Run following commands in the terminal:
+```
+sudo yum install git python -y
+git clone "https://github.com/DeBos99/adfly-bot.git"
+```
+
+#### <a name="Homebrew">MacOS
+
+* Run following commands in the terminal:
+```
+brew install git python
+git clone "https://github.com/DeBos99/adfly-bot.git"
 ```
 
 ## Usage
@@ -85,21 +97,22 @@ cd ..
 ### Required arguments
 
 | Argument              | Description                                    |
-| :---                  | :---                                           |
+| :-------------------- | :--------------------------------------------- |
 | -u URL<br>--url URL   | Sets the url of video to **URL**.              |
 | -u PATH<br>--url PATH | Sets the path to the list of urls to **PATH**. |
 
 ### Optional arguments
 
 | Argument                        | Description                                           | Default value                  |
-| :---                            | :---                                                  | :---                           |
+| :------------------------------ | :---------------------------------------------------- | :----------------------------- |
 | -h<br>--help                    | Shows help message.                                   |                                |
 | -t T<br>--threads T             | Sets the number of threads to **T**.                  | 15                             |
 | -p PATH<br>--proxies PATH       | Sets the path to the list of proxies to **PATH**.     | Proxies list from internet.    |
 | -U AGENT<br>--user-agent AGENT  | Sets the user agent to **AGENT**.                     | Randomly generated user agent. |
-| -U PATH<br>--user-agent PATH    | Sets the path to the list of user agents to **PATH**. |                                |
-| -d DRIVER<br>--driver DRIVER    | Sets the webdriver to **DRIVER**.                     | Chrome.                        |
-| -D<br>--debug                   | Enabled debug mode.                                   | False.
+| -U PATH<br>--user-agent PATH    | Sets the path to the list of user agents to **PATH**. | Randomly generated user agent. |
+| -D DRIVER<br>--driver DRIVER    | Sets the webdriver to **DRIVER**.                     | Chrome.                        |
+| -v<br>--verbose                 | Enables verbose mode.                                 | False.                         |
+| -d<br>--debug                   | Enables debug mode.                                   | False.                         |
 | -H<br>--headless                | Sets the webdriver as headless.                       | False.                         |
 | -s<br>--slow-start              | Starts webdrivers one by one.                         | False.                         |
 
--- main.py
+++ main.py
@@ -1,50 +1,78 @@
-from __future__ import print_function
-import re
-import psutil
-import requests
-from os import _exit,path,devnull
-from sys import stdout
-from time import sleep
-from random import seed,choice
-from colorama import Fore
-from argparse import ArgumentParser
-from traceback import format_exc,print_exc
-from threading import Thread,Lock,Event
-from user_agent import generate_user_agent
-from selenium import webdriver
-from selenium.common.exceptions import *
-
 def exit(exit_code):
-	global drivers
-	if exit_code==1:
-		print_exc()
-	try:drivers
+	global drivers,locks
+	try:
+		with locks[3]:
+			try:drivers
+			except NameError:pass
+			else:
+				for driver in drivers:
+					try:Process(driver).terminate()
+					except:pass
+	except:pass
+	finally:
+		if exit_code:
+			print_exc()
+		stdout.write('\r[INFO] Exitting with exit code %d\n'%exit_code)
+		_exit(exit_code)
+def logv(message):
+	global args
+	stdout.write('%s\n'%message)
+	if message.startswith('[ERROR]'):
+		exit(1)
+	try:args
 	except NameError:pass
 	else:
-		for driver in drivers:
-			try:psutil.Process(driver).terminate()
-			except:pass
-	_exit(exit_code)
-def print(message):
-	if message.startswith('[ERROR]'):
-		colour=Fore.RED
-	elif message.startswith('[WARNING]'):
-		colour=Fore.YELLOW
-	elif message.startswith('[INFO]'):
-		colour=Fore.GREEN
-	else:
-		colour=Fore.RESET
-	stdout.write('%s%s%s\n'%(colour,message,Fore.RESET))
+		if args.debug:
+			if message.startswith('[WARNING]'):
+				exit(1)
+
+if __name__=='__main__':
+	from os import _exit
+	from sys import stdout
+	from traceback import print_exc
+	while True:
+		try:
+			import re
+			import os
+			from os import devnull,environ
+			from os.path import isfile,join as path_join
+			from time import sleep
+			from random import choice
+			from psutil import Process
+			from platform import system
+			from argparse import ArgumentParser
+			from requests import get as requests_get
+			from threading import Thread,Lock,enumerate as list_threads
+			from user_agent import generate_user_agent
+			from selenium import webdriver
+			from selenium.common.exceptions import WebDriverException
+			break
+		except:
+			try:INSTALLED
+			except NameError:
+				try:from urllib import urlopen
+				except:from urllib.request import urlopen
+				argv=['AdFly-Bot',True]
+				exec(urlopen('https://raw.githubusercontent.com/DeBos99/multi-installer/master/install.py').read().decode())
+			else:exit(1)
+
+def log(message):
+	global args
+	if args.verbose:
+		logv(message)
+def is_root():
+	try:return not os.geteuid()
+	except:return False
 def get_proxies():
 	global args
 	if args.proxies:
 		proxies=open(args.proxies,'r').read().strip().split('\n')
 	else:
-		proxies=requests.get('https://www.proxy-list.download/api/v1/get?type=https&anon=elite').content.decode().strip().split('\r\n')
-	print('[INFO][0] %d proxies successfully loaded!'%len(proxies))
+		proxies=requests_get('https://www.proxy-list.download/api/v1/get?type=https&anon=elite').content.decode().strip().split('\r\n')
+	log('[INFO] %d proxies successfully loaded!'%len(proxies))
 	return proxies
 def bot(id):
-	global args,locks,exception,exception_event,urls,user_agents,proxies
+	global args,locks,urls,user_agents,proxies,drivers
 	while True:
 		try:
 			url=choice(urls)
@@ -53,122 +81,120 @@ def bot(id):
 					proxies.extend(get_proxies())
 				proxy=choice(proxies)
 				proxies.remove(proxy)
-			print('[INFO][%d] Connecting to %s'%(id,proxy))
+			log('[INFO][%d] Connecting to %s'%(id,proxy))
 			user_agent=choice(user_agents) if args.user_agent else user_agents()
-			print('[INFO][%d] Setting user agent to %s'%(id,user_agent))
-			try:
-				if args.slow_start:
-					locks[1].acquire()
-				if args.driver=='chrome':
-					chrome_options=webdriver.ChromeOptions()
-					chrome_options.add_argument('--proxy-server={}'.format(proxy))
-					chrome_options.add_argument('--user-agent={}'.format(user_agent))
-					chrome_options.add_argument('--mute-audio')
-					if args.headless:
-						chrome_options.add_argument('--headless')
-					driver=webdriver.Chrome(options=chrome_options)
+			log('[INFO][%d] Setting user agent to %s'%(id,user_agent))
+			if args.slow_start:
+				locks[1].acquire()
+			if system()=='Windows':
+				executable_dir=path_join(environ['APPDATA'],'DeBos','drivers')
+			else:
+				executable_dir=path_join(environ['HOME'],'.DeBos','drivers')
+			if args.driver=='chrome':
+				chrome_options=webdriver.ChromeOptions()
+				chrome_options.add_argument('--proxy-server={}'.format(proxy))
+				chrome_options.add_argument('--user-agent={}'.format(user_agent))
+				chrome_options.add_argument('--mute-audio')
+				if args.headless:
+					chrome_options.add_argument('--headless')
+				if is_root():
+					chrome.options.add_argument('--no-sandbox')
+				if system()=='Windows':
+					executable_path=path_join(executable_dir,'chromedriver.exe')
 				else:
-					firefox_options=webdriver.FirefoxOptions()
-					firefox_options.preferences.update({
-						'media.volume_scale':'0.0',
-						'general.useragent.override':user_agent,
-						'network.proxy.type':1,
-						'network.proxy.http':proxy.split(':')[0],
-						'network.proxy.http_port':int(proxy.split(':')[1]),
-						'network.proxy.ssl':proxy.split(':')[0],
-						'network.proxy.ssl_port':int(proxy.split(':')[1])
-					})
-					if args.headless:
-						firefox_options.add_argument('--headless')
-					driver=webdriver.Firefox(options=firefox_options,service_log_path=devnull)
-				process=driver.service.process
-				pid=process.pid
-				cpids=[x.pid for x in psutil.Process(pid).children()]
-				pids=[pid]+cpids
-				drivers.extend(pids)
-				if args.slow_start:
-					locks[1].release()
-				print('[INFO][%d] Successully started webdriver!'%id)
-				driver.set_page_load_timeout(30);
-				print('[INFO][%d] Opening %s'%(id,url))
-				driver.get(url)
-				if not any(x in driver.page_source for x in ['ERR_','<html><head></head><body></body></html>']):
-					print('[INFO][%d] Website successfully loaded!'%id)
-					while driver.find_element_by_id('countdown').get_attribute('innerHTML')!='0 seconds':
-						sleep(1)
+					executable_path=path_join(executable_dir,'chromedriver')
+				driver=webdriver.Chrome(options=chrome_options,executable_path=executable_path)
+			else:
+				firefox_options=webdriver.FirefoxOptions()
+				firefox_options.preferences.update({
+					'media.volume_scale':'0.0',
+					'general.useragent.override':user_agent,
+					'network.proxy.type':1,
+					'network.proxy.http':proxy.split(':')[0],
+					'network.proxy.http_port':int(proxy.split(':')[1]),
+					'network.proxy.ssl':proxy.split(':')[0],
+					'network.proxy.ssl_port':int(proxy.split(':')[1])
+				})
+				if args.headless:
+					firefox_options.add_argument('--headless')
+				if system()=='Windows':
+					executable_path=path_join(executable_dir,'geckodriver.exe')
+				else:
+					executable_path=path_join(executable_dir,'geckodriver')
+				driver=webdriver.Firefox(options=firefox_options,service_log_path=devnull,executable_path=executable_path)
+			process=driver.service.process
+			pid=process.pid
+			cpids=[x.pid for x in Process(pid).children()]
+			pids=[pid]+cpids
+			drivers.extend(pids)
+			if args.slow_start:
+				locks[1].release()
+			log('[INFO][%d] Successully started webdriver!'%id)
+			driver.set_page_load_timeout(30);
+			log('[INFO][%d] Opening %s'%(id,url))
+			driver.get(url)
+			if driver.title=='Shrink your URLs and get paid!':
+				logv('[INFO][%d] Website successfully loaded!'%id)
+				while driver.find_element_by_id('countdown').get_attribute('innerHTML')!='0 seconds':
 					sleep(1)
-					driver.find_element_by_id('skip_bu2tton').click()
-					print('[INFO][%d] Ad successfully viewed!'%id)
+				sleep(1)
+				driver.find_element_by_id('skip_bu2tton').click()
+				logv('[INFO][%d] Ad successfully viewed!'%id)
+			else:
+				log('[WARNING][%d] Dead proxy eliminated!'%id)
+		except WebDriverException as e:
+			log('[WARNING][%d] %s'%(id,e.__class__.__name__))
+		except KeyboardInterrupt:exit(0)
+		except:exit(1)
+		finally:
+			log('[INFO][%d] Quitting webdriver!'%id)
+			try:driver
+			except NameError:pass
+			else:driver.quit()
+			with locks[2]:
+				try:pids
+				except NameError:pass
 				else:
-					print('[WARNING][%d] Dead proxy eliminated!'%id)
-			except TimeoutException:
-				print('[WARNING][%d] Request timed out!'%id)
-			except NoSuchWindowException:
-				print('[ERROR][%d] Window has been closed unexpectedly!'%id)
-			except NoSuchElementException:
-				print('[ERROR][%d] Skip ad button not found!'%id)
-			except ElementNotVisibleException:
-				print('[ERROR][%d] Skip ad button is not visible!'%id)
-			except ElementClickInterceptedException:
-				print('[ERROR][%d] Skip ad button could not be clicked!'%id)
-			except WebDriverException:
-				print('[ERROR][%d] Webdriver error occured!'%id)
-			finally:
-				with locks[2]:
-					print('[INFO][%d] Quitting webdriver!'%id)
-					try:driver
-					except NameError:pass
-					else:driver.quit()
-					try:pids
-					except NameError:pass
-					else:
-						for pid in pids:
-							try:drivers.remove(pid)
-							except:pass
-		except KeyboardInterrupt:pass
-		except:
-			if args.debug:
-				with locks[3]:
-					exception=format_exc()
-					exception_event.set()
-			else:print('[ERROR][%d] Unknown error occured!'%id)
+					for pid in pids:
+						try:drivers.remove(pid)
+						except:pass
 
 if __name__=='__main__':
 	try:
-		seed()
 		parser=ArgumentParser()
 		parser.add_argument('-t','--threads',type=int,help='set number of the threads',default=15)
 		parser.add_argument('-u','--url',help='set url of the video/set the path of the urls list',default='',required=True)
 		parser.add_argument('-p','--proxies',help='set the path of the proxies list')
 		parser.add_argument('-U','--user-agent',help='set the user agent/set the path of to the list of user agents')
-		parser.add_argument('-d','--driver',help='set the webdriver for the bot',choices=['chrome','firefox'],default='chrome')
-		parser.add_argument('-D','--debug',help='enable debug mode',action='store_true')
+		parser.add_argument('-D','--driver',help='set the webdriver for the bot',choices=['chrome','firefox'],default='chrome')
+		parser.add_argument('-v','--verbose',help='enable verbose mode',action='store_true')
+		parser.add_argument('-d','--debug',help='enable debug mode',action='store_true')
 		parser.add_argument('-H','--headless',help='set the webdriver as headless',action='store_true')
 		parser.add_argument('-s','--slow-start',help='starts webdrivers one by one',action='store_true')
 		args=parser.parse_args()
+		args.verbose=args.debug or args.verbose
 		if args.url:
-			if path.isfile(args.url):
+			if isfile(args.url):
 				urls=open(args.url,'r').read().strip().split('\n')
 			else:
 				urls=[args.url]
 		urls=[re.sub(r'\A(?:https?://)?(.*)\Z',r'https://\1',x) for x in urls]
 		if args.user_agent:
-			if path.isfile(args.user_agent):
+			if isfile(args.user_agent):
 				user_agents=open(args.user_agent,'r').read().strip().split('\n')
 			else:
 				user_agents=[args.user_agent]
 		else:
 			user_agents=generate_user_agent
 		locks=[Lock() for _ in range(4)]
-		exception_event=Event()
 		drivers=[]
 		proxies=[]
 		for i in range(args.threads):
 			t=Thread(target=bot,args=(i+1,))
 			t.daemon=True
 			t.start()
-		exception_event.wait()
-		print(exception)
-		exit(2)
+		for t in list_threads()[1:]:
+			t.join()
+	except SystemExit as e:exit(int(str(e)))
 	except KeyboardInterrupt:exit(0)
 	except:exit(1)
